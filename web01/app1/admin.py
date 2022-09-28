a4e2a771e2bf74cda3fd16103aa2af24af66db56
zaidkamil <xaidmetamorphos@gmail.com>
2022-09-28 18:54:07 +0530

    admin and model

--- web01/app1/admin.py
+++ web01/app1/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Movies, Show
+from .models import Movies, Show, Student, Report, Weather
 
 admin.site.register(Movies) # this line add ur table to admin dashboard
 admin.site.register(Show)
+
+# admiview to create admin template for models
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('rollno','klass','name')
+    search_fields = ('name','klass')
+    ordering = ('name',)
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('student','english','hindi','maths','science',
+        'socialscience','computer')
+
+@admin.register(Weather)
+class WeatherAdmin(admin.ModelAdmin):
+    list_display = ('date','temp','humidity','wind_speed')
+    list_filter = ('date',)
+    ordering = ('date',)
--- web01/app1/models.py
+++ web01/app1/models.py
@@ -18,7 +18,37 @@ class Show(Model):
     def __str__(self):
         return f'{self.title}({self.year})'
 
+class Student(Model):
+    name = CharField(max_length=50)
+    klass = IntegerField(verbose_name='class')
+    rollno = IntegerField(verbose_name='roll number')
 
+    def __str__(self):
+        return self.name
+
+class Report(Model):
+    english = IntegerField()
+    maths = IntegerField()
+    science = IntegerField()
+    hindi = IntegerField()
+    socialscience = IntegerField()
+    computer = IntegerField()
+    student = ForeignKey('Student', on_delete=DO_NOTHING)
+
+    def __str__(self) -> str:
+        return f'Report of {self.student.name}'
+
+class Weather(Model):
+    temp = DecimalField(verbose_name="Temp(C)", max_digits=5, decimal_places=2)
+    wind_speed = DecimalField(max_digits=5, decimal_places=2)
+    humidity = DecimalField(max_digits=5, decimal_places=2)
+    date = DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.temp
+
+# python manage.py makemigrations
+# python manage.py migrate
 
 
 
