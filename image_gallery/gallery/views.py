26b20034cbf1e4f400c4ccf67b411196cb930392
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-09 18:06:11 +0530

    deleting content

--- image_gallery/gallery/views.py
+++ image_gallery/gallery/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Image # (important) get the class of Category & Image
+import os
 
 def index(request):
     categories = Category.objects.all() # get all the categories
@@ -17,8 +18,14 @@ def add_category(request):
 def add_image(request):
     pass
 
-def view_image(request,image_id):
+def view_image(request, image_id):
     pass
 
-def delete_image(request,image_id):
-    pass
+def delete_image(request, image_id):
+    try:
+        item = Image.objects.get(id=image_id)   # get the image using the id
+        os.remove(item.image.path)              # delete the image file from media folder
+        item.delete()                           # delete the image from database
+    except:
+        print('Error deleting image')
+    return redirect('index')
