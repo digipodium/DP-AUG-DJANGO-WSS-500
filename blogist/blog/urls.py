c81a30e831fecb31dceb72ea94b5c449df6bbb1c
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-16 17:18:08 +0530

    added a menu bar

--- blogist/blog/urls.py
+++ blogist/blog/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import *
 
-urlpatterns = []
+urlpatterns = [
+    path('', index, name='home'),
+]
--- blogist/blog/views.py
+++ blogist/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from matplotlib.pyplot import title
 
 # Create your views here.
+
+def index(request):
+    ctx = {}
+    
+    ctx['title'] = 'Home'
+    return render(request, 'blog/index.html', ctx)
