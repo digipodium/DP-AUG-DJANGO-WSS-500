e6610ac226bd00516a1166ee0d126d353e15a219
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-22 15:02:00 +0530

    added a tag model

--- blogist/blog/models.py
+++ blogist/blog/models.py
@@ -1,3 +1,15 @@
+from enum import unique
+from tabnanny import verbose
 from django.db import models
+from django.conf import settings
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+
