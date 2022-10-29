c85132887911e999fca13f4f8a162bb02ca6df57
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-29 17:56:38 +0530

    created  blog admin #1

--- blogist/blog/admin.py
+++ blogist/blog/admin.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+from .models import Category, Tag, Image, Article
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Image)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','author','status','category')
+    list_filter = ('author','category','status')
+    search_fields = ('title','author__username','category__name')
+    date_hierarchy = 'publish_date'
+    ordering = ('created_on','publish_date')
