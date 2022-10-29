56e017ef91158c455dca48d061ddd0fcac2a2a26
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-29 19:05:28 +0530

    create template with form #1

--- blogist/blog/urls.py
+++ blogist/blog/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', landing, name='landing'),
     path('home/', index, name='home'),
+    path('article/new/', article_create, name='create'),
 ]
--- blogist/blog/views.py
+++ blogist/blog/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from .forms import *
+from django.contrib import messages
+from django.conf import settings
 
 def index(request):
     ctx = {}
@@ -9,3 +13,27 @@ def landing(request):
     ctx = {}
     ctx['title'] = 'Welcome to Blogist'
     return render(request, 'blog/landing.html', ctx)
+
+def article_create(request):
+    ctx = {'title': 'Create Article | Blogist'}
+    article_form = ArticleForm(request.POST or None)
+    category_form = CategoryForm()
+    tag_form = TagForm()
+    image_form = ImageForm()
+    if article_form.is_valid():
+        pass
+    ctx['article_form'] = article_form
+    ctx['category_form'] = category_form
+    ctx['tag_form'] = tag_form
+    ctx['image_form'] = image_form
+    return render(request, 'blog/create.html', ctx)
+
+# ajax base views
+def category_create(request):
+    pass
+
+def tag_create(request):
+    pass
+
+def image_upload(request):
+    pass
