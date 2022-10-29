46fe44814e7d978e4fba3da8ed1d607aa49bb1b1
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-29 17:43:05 +0530

    created blog models #2

--- blogist/blog/models.py
+++ blogist/blog/models.py
@@ -1,15 +1,47 @@
-from enum import unique
-from tabnanny import verbose
 from django.db import models
 from django.conf import settings
+# pip install `django-tinymce` for the import below
 from tinymce.models import HTMLField
 
-# Create your models here.
-class Tag(models.Model):
-    name = models.CharField(max_length=255)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 
+class Image(models.Model):
+    image = models.ImageField(upload_to='articles/images/')
+    caption = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)    
+
+    def __str__(self):
+        return self.image.url
+
+class Article(models.Model):
+
+    class StatusChoice(models.IntegerChoices):
+        DRAFT = 0, 'Draft'
+        PUBLISHED = 1, 'Published'
+    
+    title = models.CharField(max_length=100, unique=True)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
+    status = models.IntegerField(choices=StatusChoice.choices, default=StatusChoice.DRAFT)
+    tags = models.ManyToManyField(Tag)
+    duration = models.DurationField()
+    content = HTMLField()
+    images = models.ManyToManyField(Image)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
--- blogist/blog/views.py
+++ blogist/blog/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     ctx = {}
-    
     ctx['title'] = 'Home'
-    
     return render(request, 'blog/index.html', ctx)
 
 def landing(request):
