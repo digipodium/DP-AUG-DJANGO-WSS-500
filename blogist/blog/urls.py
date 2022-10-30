4d45d1d560fe7cea8af621c1247ce3168ee45b5d
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-30 18:52:38 +0530

    ajax form submission

--- blogist/blog/urls.py
+++ blogist/blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', landing, name='landing'),
     path('home/', index, name='home'),
     path('article/new/', article_create, name='create'),
+    path('api/category/create/', category_create, name='cat_create'),
+    path('api/tag/create/', tag_create, name='tag_create'),
 ]
--- blogist/blog/views.py
+++ blogist/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
@@ -21,7 +22,13 @@ def article_create(request):
     tag_form = TagForm()
     image_form = ImageForm()
     if article_form.is_valid():
-        pass
+        # print("Form data")
+        # print(article_form.cleaned_data)
+        article = article_form.save(commit=False) # save the form partially
+        article.author = request.user 
+        article.save()
+        messages.success(request, 'Article created successfully')  
+
     ctx['article_form'] = article_form
     ctx['category_form'] = category_form
     ctx['tag_form'] = tag_form
@@ -30,10 +37,28 @@ def article_create(request):
 
 # ajax base views
 def category_create(request):
-    pass
+    if request.method == 'POST':
+        category_form = CategoryForm(request.POST)
+        if category_form.is_valid():
+            cat = category_form.save(commit=False)
+            cat.save()
+            return JsonResponse({'status': 'success', 'name': cat.name, 'id': cat.id})
+        else:
+            return JsonResponse({'status': 'invalid data'})
+    else:
+        return JsonResponse({'status': 'error'})
 
 def tag_create(request):
-    pass
+    if request.method == 'POST':
+        tag_form = TagForm(request.POST)
+        if tag_form.is_valid():
+            tag = tag_form.save(commit=False)
+            tag.save()
+            return JsonResponse({'status': 'success', 'name': tag.name, 'id': tag.id})
+        else:
+            return JsonResponse({'status': 'invalid data'})
+    else:
+        return JsonResponse({'status': 'error'})
 
 def image_upload(request):
     pass
